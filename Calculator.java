28e7fd85212ebaf21ae31466d5c52b6dea5a2e28
eunice <eunicewanjiru208@gmail.com>
2024-02-12 21:53:44 +0300

    Import java inorder to access the Scanner. Also name the public class.

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    // public static void main(String[] args) {
+    //     int number1, number2, sum, mul, div;
+    //     Scanner fred = new Scanner(System.in);
+
+    //     System.out.println("Enter First Number:");
+    //     number1 = fred.nextInt();
+
+    //     System.out.println("Enter Second Number:");
+    //     number2 = fred.nextInt();
+
+    //     sum = number1 + number2;
+    //     System.out.println("Sum is " + sum);
+         
+    //     mul = number1*number2;
+    //     System.out.println("mul is " + mul);
+        
+    //     div = number1/number2;
+    //     System.out.println("div is " + div);
+
+       
+    // }
+}
